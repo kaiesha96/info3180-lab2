da1b1927481fef2fa14d28e8415473859806194e
Shaniel Wallace <shaniel.wallace@mymona.uwi.edu>
2018-02-12 00:47:12 +0000

    Lab 2 update

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import date
 
 ###
 # Routing for your application.
@@ -22,13 +22,22 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Shaniel Wallace")
 
-
+@app.route("/profile")
+def profile():
+    """Render the website's profile page"""
+    return render_template('profile.html', name="Shaniel Wallace", uname="kaiesha96",location="Kingston,Jamaica",posts=30,following=5,followers="8.5k",joined=format_date_joined())
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
+def format_date_joined():
+    """Formats the date joined"""
+    d = date.today();
+    return "Joined {0:%b} {0:%Y}".format(d)
+    
+    
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
@@ -53,6 +62,6 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
-
+    
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
